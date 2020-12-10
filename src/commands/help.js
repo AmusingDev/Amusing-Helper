5882aee5b960e92e98fcbfa307b46dcb76e2bae6
James <59822256+AmusingDev@users.noreply.github.com>
2020-12-10 23:13:39 +0000

    Create help.js

---
+++ src/commands/help.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
+const botconfig = require('./../botconfig.json');
+const prefix = botconfig.prefix;
+
+module.exports.run = async (client, msg, args) => {
+
+	if (args[0]) return;
+
+	msg.delete();
+	const help = new MessageEmbed()
+
+		.setColor(botconfig.embedcolor)
+		.setAuthor('Amusing Helper', client.user.avatarURL())
+		.setThumbnail(client.user.avatarURL())
+		.setDescription("Amusing Helper is an open-source project that has been created for beginner bot developers as a learning tool!")	
+
+	const errorEmbed = new MessageEmbed()
+
+		.setColor(botconfig.errorColor)
+		.setTitle('Error!')
+		.setDescription("I am not able to DM you, please check if '**Allow direct messages from server members**' is enabled or if you've blocked the bot")
+
+	msg.author.send(help).catch(() => msg.channel.send(errorEmbed))
+}
+
+module.exports.command = {
+	name: "help",
+    aliases: []
+}
