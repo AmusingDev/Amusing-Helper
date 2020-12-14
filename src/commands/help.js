8e29fe7b22e9260e7dc8488d5a45df0d5b2e9964
James <59822256+AmusingDev@users.noreply.github.com>
2020-12-14 19:57:10 +0000

    Added Comments
    Labelled most of the code with comments and replace 'botconfig' with 'config' to simplify the code.

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,30 +1,29 @@
 const Discord = require('discord.js');
 const { Client, MessageEmbed } = require('discord.js');
-const botconfig = require('./../botconfig.json');
-const prefix = botconfig.prefix;
+const config = require('./../config.json');
 
 module.exports.run = async (client, msg, args) => {
 
-	if (args[0]) return;
+	if (args[0]) return; // If there are any arguments the command will not work
 
 	msg.delete();
 	const help = new MessageEmbed()
 
-		.setColor(botconfig.embedcolor)
-		.setAuthor('Amusing Helper', client.user.avatarURL())
-		.setThumbnail(client.user.avatarURL())
-		.setDescription("Amusing Helper is an open-source project that has been created for beginner bot developers as a learning tool!")	
+		.setColor(botconfig.embedcolor) // The color of the embed (left line)
+		.setAuthor('Amusing Helper', client.user.avatarURL()) // Author line - ("Text", image)
+		.setThumbnail(client.user.avatarURL()) // Image on the right side
+		.setDescription("Amusing Helper is an open-source project that has been created for beginner bot developers as a learning tool!") // Main text
 
-	const errorEmbed = new MessageEmbed()
+	const errembed = new MessageEmbed()
 
 		.setColor(botconfig.errorcolor)
-		.setTitle('Error!')
+		.setTitle('Error!') // Large title text
 		.setDescription("I am not able to DM you, please check if '**Allow direct messages from server members**' is enabled or if you've blocked the bot")
 
-	msg.author.send(help).catch(() => msg.channel.send(errorEmbed))
+	msg.author.send(help).catch(() => msg.channel.send(errembed)); // Sends the embed to the author's DMs. If the author's DMs are closed, the bot will send the error 																		  embed in the chat
 }
 
 module.exports.command = {
-	name: "help",
-    aliases: []
+	name: "help", // What the user has to say to trigger the command
+    aliases: [] // Command alternatives
 }
