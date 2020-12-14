7bf91eaac8862cf6e51d474bc893634fe87b2226
James <59822256+AmusingDev@users.noreply.github.com>
2020-12-14 23:18:47 +0000

    Created ping.js
    Responds with the bot's latency when executed.
    
    Command: !ping
    Aliases: !latency

---
+++ src/commands/ping.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, msg, args) => {
+
+	if (args[0]) return; // If there are any arguments the command will not work
+    let latency = Date.now() - msg.createdTimestamp;
+    let api_latency = Math.round(client.ws.ping);
+    
+    msg.channel.send(`**Latency**: \`${latency}ms\`\n**API Latency**: \`${api_latency}ms\``).catch(msg.channel.send("An error has occured!"));
+
+}
+
+module.exports.command = {
+	name: "ping", // What the user has to say to trigger the command
+    aliases: ["latency"] // Command alternatives
+}
